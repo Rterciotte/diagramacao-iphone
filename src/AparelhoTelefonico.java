fe498e68d5b9170b7b2f2e66ab9300ce1037d920
Rogério Terciotte <rterciotte@gmail.com>
2023-08-27 02:46:07 -0300

    Project completed

---
+++ src/AparelhoTelefonico.java
@@ -0,0 +1,16 @@
+public class AparelhoTelefonico {
+
+    private String numero;
+
+    public void ligar(String numero) {
+        System.out.println("Ligando para o número: " + numero);
+    }
+
+    public void atender() {
+        System.out.println("Atendendo Ligação...");
+    }
+
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando Correio de Voz...");
+    }
+}
---
+++ src/ReprodutorMidia.java
@@ -0,0 +1,6 @@
+public interface ReprodutorMidia {
+
+    public void tocar();
+    public void pausar();
+    public void selecionarMusica();
+}
